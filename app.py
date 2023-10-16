5063aa16bcea2a7dd2e2450a3fde6698bcc38642
Zack Adams <imzackadams@protonmail.com>
2023-10-16 04:06:33 -0400

    idk what happened here

--- app.py
+++ app.py
@@ -6,9 +6,11 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///emails.db'
 app.config['SECRET_KEY'] = 'your_secret_key_here'
 db = SQLAlchemy(app)
 
+
 class Email(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -17,12 +19,24 @@ def index():
         if not email:
             flash('Email is required!')
             return redirect('/')
+
+        existing_email = Email.query.filter_by(email=email).first()
+        if existing_email:
+            flash('Email already subscribed!')
+            return redirect('/')
+
         new_email = Email(email=email)
         db.session.add(new_email)
-        db.session.commit()
-        flash('Thanks for subscribing!')
+        try:
+            db.session.commit()
+            flash('Thanks for subscribing!')
+        except:
+            db.session.rollback()
+            flash('An error occurred. Please try again.')
+
         return redirect('/')
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     with app.app_context():
