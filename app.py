6ddc82761586f8c5b25312a3c5200a5b82a5a052
Zack Adams <imzackadams@protonmail.com>
2023-10-16 00:42:46 -0400

    BROKEN flask server

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, redirect, render_template, flash
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///emails.db'
+app.config['SECRET_KEY'] = 'your_secret_key_here'
+db = SQLAlchemy(app)
+
+
+class Email(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        if not email:
+            flash('Email is required!')
+            return redirect('/')
+        new_email = Email(email=email)
+        db.session.add(new_email)
+        db.session.commit()
+        flash('Thanks for subscribing!')
+        return redirect('/')
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
